3cf2f180633966b5663b6846d6fe40a8d93680a2
GinoongR0KI <mail.roqueperez@gmail.com>
2023-01-06 22:42:20 +0800

    Principal Approval on Lessons (Cont.)
    + updated Lessons manage page

--- Lessons/manage.php
+++ Lessons/manage.php
@@ -26,6 +26,9 @@ $redir->unAuth("Student", "../");
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../css/style.min.css">
     <link rel="stylesheet" href="../css/style.css">
+
+    <link rel="manifest" href="../manifest.json">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -43,6 +46,7 @@ $redir->unAuth("Student", "../");
 
     <script src="AJAX/Admin/createLesson.js"></script>
     <script src="AJAX/Admin/deleteLesson.js"></script>
+    <script src="AJAX/Admin/approveLesson.js"></script>
 
     <script src="../javascript/toaster.js"></script>
     <script src="../javascript/rower.js"></script>
